ca8f4caeba6c1543f73860e7efd5b6ee1f201523
Pedro Moreira <pedrohenryque.rm@gmail.com>
2026-05-06 21:01:41 +0100

    tidying up the proto file

--- server/app.js
+++ server/app.js
@@ -8,10 +8,9 @@ const quizList = require('./assets/quiz_questions.js');
 console.log({studentsList})
 console.log({quizList})
 
-let professorStream = null; // To hold the professor's stream for monitoring quiz and attendance status. 
-
-  // console.log()
-
+// create professorStream variable to hold the professor's stream for monitoring quiz and attendance status.
+let professorAttendenceStream = null;
+let professorQuizStream = null;
 // Path to proto file
 const PROTO_PATHS = [
     path.join(__dirname, './proto/education.proto'),
@@ -26,16 +25,11 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATHS, {
 });
 const educationProto = grpc.loadPackageDefinition(packageDefinition).education;
 
-// Professor connection for monitoring quiz and attendance status
+// Professor connection for monitoringattendance status
 const ProfessorAttendenceTracker =  (call) => {
-    professorStream = call; // Store the stream for later use 
+    professorAttendenceStream = call; // Store the stream for later use 
 
     call.write({ 
-//           string student_id = 1;
-//   string student_name = 2;
-//   string student_location = 3;
-//   string student_attendance_status = 4;
-//   string message = 5;
         student_id: "N/A",
         student_name: "N/A",
         student_location: "N/A",
@@ -62,7 +56,7 @@ const ProfessorAttendenceTracker =  (call) => {
 
 
 // Student Check-in Service
-function SendAttenceConfirmation(call, callback) {
+const SendAttenceConfirmation = (call, callback) =>{
 
   const studentId = call.request.student_id;
   let response = "";
@@ -93,12 +87,58 @@ function SendAttenceConfirmation(call, callback) {
 
 }
 
+const ProfessorQuizTracker = (call) => { 
+    //Professor request quiz to be active, and the server will send real-time updates on quiz status and student submissions.
+    professorQuizStream = call; // Store the stream for later use
+    const professorId = call.request.professor_id;
+
+    console.log(`Professor ${professorId} connected to quiz tracker.`);
+    const quizId = call.request.quiz_id;
+
+    //find the quiz in the list
+    // check if quiz is active, if is not, then activate, otherwise say quiz is already active. then send real-time updates on quiz status and student submissions.
+    const quiz = quizList.quiz_list.find(q => q.id === parseInt(quizId));
+    if (quiz) {
+        if(quiz.status === 'active') {
+            call.write({
+                student_id: "N/A",
+                student_name: "N/A",
+                quiz_status: 'Active',
+                message: `Quiz ${quiz.title} is already active.`    
+            });
+        } else {
+            quiz.status = 'active';
+            call.write({
+
+                student_id: "N/A",
+                student_name: "N/A",
+                quiz_status: 'Active',
+                message: `Quiz ${quiz.title} is now active.`
+            });
+        }
+    }   
+    
+    call.on('end', () => {
+        console.log('Professor disconnected from quiz tracker.');
+        professorQuizStream = null; // Clear the stream when professor disconnects
+        call.end();
+    }
+    );
+
+    call.on('error', (err) => {
+        console.error('Error in ProfessorQuizTracker stream:', err);
+        professorQuizStream = null; // Clear the stream on error
+    });
+
+
+}
 
 const server = new grpc.Server();
 
 server.addService(educationProto.EducationService.service, {
   SendAttenceConfirmation: SendAttenceConfirmation,
-  ProfessorAttendenceTracker: ProfessorAttendenceTracker
+  ProfessorAttendenceTracker: ProfessorAttendenceTracker,
+  ProfessorQuizTracker: ProfessorQuizTracker
 });
 
 // Start server
--- server/assets/quiz_questions.js
+++ server/assets/quiz_questions.js
@@ -6,32 +6,32 @@ const quiz_list = [{
             {
                 question_id: 1,
                 question: "Who was the first President of the United States?",
-                options: ["George Washington", "Thomas Jefferson", "Abraham Lincoln", "John Adams"],
-                correct: "George Washington"
+                options: [{title: "George Washington", id: 1}, {title: "Thomas Jefferson", id: 2}, {title: "Abraham Lincoln", id: 3}, {title: "John Adams", id: 4}],
+                correct: {title: "George Washington", id: 1}    
             },
             {   
                 question_id: 2,
                 question: "In which year did World War II end?",
-                options: ["1940", "1945", "1918", "1939"],
-                correct: "1945"
+                options: [{title: "1940", id: 1}, {title: "1945", id: 2}, {title: "1918", id: 3}, {title: "1939", id: 4}],
+                correct: {title: "1945", id: 2}
             },
             {
                 question_id: 3,
                 question: "Which ancient civilization built Machu Picchu?",
-                options: ["Aztec", "Maya", "Inca", "Olmec"],
-                correct: "Inca"
+                options: [{title: "Aztec", id: 1}, {title: "Maya", id: 2}, {title: "Inca", id: 3}, {title: "Olmec", id: 4}],
+                correct: {title: "Inca", id: 3}
             },
             {
                 question_id: 4,
                 question: "Who discovered the sea route to India in 1498?",
-                options: ["Christopher Columbus", "Vasco da Gama", "Ferdinand Magellan", "James Cook"],
-                correct: "Vasco da Gama"
+                options: [{title: "Christopher Columbus", id: 1}, {title: "Vasco da Gama", id: 2}, {title: "Ferdinand Magellan", id: 3}, {title: "James Cook", id: 4}],
+                correct: {title: "Vasco da Gama", id: 2}
             },
             {
                 question_id: 5,
                 question: "The French Revolution began in which year?",
-                options: ["1789", "1776", "1812", "1804"],
-                correct: "1789"
+                options: [{title: "1789", id: 1}, {title: "1776", id: 2}, {title: "1812", id: 3}, {title: "1804", id: 4}    ],
+                correct: {title: "1789", id: 1}
             }
     ]
 }
